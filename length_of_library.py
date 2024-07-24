483eb80767ed93373d0fa6ab8fa4654b4b186198
Joshcubes <joshuapoynton@icloud.com>
2024-07-24 21:45:01 +0100

    + Finish total length of library

--- length_of_library.py
+++ length_of_library.py
@@ -1,18 +1,81 @@
+from math import remainder
+
+
+global total_length
+total_length = 0.0
+
 def run(library_path):
     from pathlib import Path
-    
-    print("Working")
+    from mutagen.mp3 import MP3
 
+    print("Working")
+    
+    # Iterates over all subfolders looking for mp3 files
     def process_folders(rootfolder):
+        global total_length
         folder = Path(rootfolder)
+        
         for item in folder.iterdir():
             if item.is_dir():
                 # recursion for subfolder
                 process_folders(item)
             elif item.suffix == ".mp3":
-                process_file(item)
-    
-    def process_file(file):
-        print(file)
-    
+                # opens the file and gets the length
+                audio = MP3(item)
+                total_length += audio.info.length
+
     process_folders(library_path)
+    print(f"Your library has a total length of {total_length} Secconds.")
+    
+    print("Which is:")
+    remainder_length = total_length
+    time_days = 0
+    time_hours = 0
+    time_minutes = 0
+    time_secconds = 0.0
+    #Days
+    if total_length >= 86400:
+        time_days = remainder_length // 86400
+        remainder_length -= (time_days * 86400)
+
+        time_hours = remainder_length // 3600
+        remainder_length -= (time_hours * 3600)
+        
+        time_minutes = remainder_length // 60
+        remainder_length -= (time_minutes * 60)
+        
+        time_secconds = remainder_length
+        
+        print(int(time_days), "Days")
+        print(int(time_hours), "Hours")
+        print(int(time_minutes), "Minutes")
+        print(round(time_secconds,2), "Secconds")
+    # Hours
+    elif total_length >= 3600:
+        time_hours = remainder_length // 3600
+        remainder_length -= (time_hours * 3600)
+        
+        time_minutes = remainder_length // 60
+        remainder_length -= (time_minutes * 60)
+        
+        time_secconds = remainder_length
+    
+        print(int(time_hours), "Hours")
+        print(int(time_minutes), "Minutes")
+        print(round(time_secconds,2), "Secconds")
+    
+    # Minutes
+    elif total_length >= 60:
+        time_minutes = remainder_length // 60
+        remainder_length -= (time_minutes * 60)
+        
+        time_secconds = remainder_length
+        
+        print(int(time_minutes), "Minutes")
+        print(round(time_secconds,2), "Secconds")
+    
+    # Secconds
+    else:
+        time_secconds = remainder_length
+
+        print(round(time_secconds,2), "Secconds")
